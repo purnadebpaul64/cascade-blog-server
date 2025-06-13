e34522990009fc335cdb2b08aba925688f21a115
purnadebpaul64 <purnadebpaul64@gmail.com>
2025-06-13 17:22:51 +0600

    blog add api added

--- index.js
+++ index.js
@@ -2,11 +2,48 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // =========== database connection
+    const database = client.db("cascadeblog");
+    const blogCollection = database.collection("blogs");
+
+    // =========== api end points
+
+    // ==== add single blog
+    app.post("/add-blog", async (req, res) => {
+      const newBlog = req.body;
+      const result = await blogCollection.insertOne(newBlog);
+      res.status(201).send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("welcome to the CascadeBlog server.......");
